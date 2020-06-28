1e61ac5b47b8da1289fa9f04bb3ad78cb1135f6b
Marcos Rachid <marcosrachid@gmail.com>
2020-06-28 18:45:52 -0300

    miner socket handler

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"go-cryptocurrency/internal/handler"
 	"log"
+	"os"
 
+	"go-cryptocurrency/internal/handler"
+	"go-cryptocurrency/internal/models"
+	"go-cryptocurrency/internal/network"
+
+	"github.com/gin-gonic/gin"
+	gosocketio "github.com/graarh/golang-socketio"
+	"github.com/graarh/golang-socketio/transport"
 	"github.com/joho/godotenv"
 )
+
+var Blockchain []models.Block
 
 func main() {
 	err := godotenv.Load()
@@ -17,4 +26,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	r := gin.Default()
+	s := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Handle("GET", "/socket.io", network.MinerHandler(s))
+	r.Handle("POST", "/socket.io", network.MinerHandler(s))
+	r.Handle("WS", "/socket.io", network.MinerHandler(s))
+	r.Handle("WSS", "/socket.io", network.MinerHandler(s))
+	go r.Run(":" + os.Getenv("REST_PORT"))
 }
